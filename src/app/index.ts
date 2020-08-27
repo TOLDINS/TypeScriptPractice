ef3c5876f85fe3d797250489c0c07ebb72408320
toldins <rudiak.denchik@gmail.com>
2020-08-27 11:11:52 +0300

    First routing

---
+++ src/app/index.ts
@@ -0,0 +1,13 @@
+import express = require('express');
+import {Request,Response,Application} from 'express';
+import * as bodyParser from 'body-parser';
+const app:Application= express();
+const port=8080;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.get('/',(req:Request,res:Response)=>{
+    res.send('Welcome');
+})
+app.listen(port,()=>{
+    console.log('Server has been started!');
+})
